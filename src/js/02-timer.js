10265aa7f59e89d5b9e88e5881d9f1e427d7e7f4
OlyaNegrych <olyanegrych@gmail.com>
2022-08-10 16:58:22 +0300

    added styles to task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const timerRef = document.querySelector('.timer');
+const fieldRef = document.querySelector('.field');
+const valueRef = document.querySelector('.value');
+const labelRef = document.querySelector('.label');
+
+// timerRef.style.display = "flex";
+// fieldRef.style.display = "flex";
+// fieldRef.style.flexDirection = 'column';
+// fieldRef.style.justifyContent = 'center';
+
+// valueRef.style.display = 'block';
+// labelRef.style.display = 'block';
